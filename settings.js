c0944d4a7e1c22155d16f2603a6cb41af2140882
Melo484 <mensahgideon108@gmail.com>
2026-02-01 09:42:42 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Melo's Bot",
+  botOwner: 'Melo', // Your name
+  ownerNumber: '233597613455', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
